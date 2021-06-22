51e05067cb00e9047783b3bc7f7894dfe87bb897
Rangel Netto <rangelnetto@sympla.com.br>
2021-06-22 10:46:55 -0300

    adding stripe checkout page

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,40 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-const SubscribeButton = ({ priceId }: SubscribeButtonProps) => (
-  <button
-    type="button"
-    className={styles.subscribeButton}
-  >
-    Subscribe now
-  </button>
-)
+const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const [session] = useSession()
+
+  const handeSubscribe = async () => {
+    if(!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data
+      const stripe = await getStripeJs()
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      console.error({ error })      
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handeSubscribe}
+    >
+      Subscribe now
+    </button>
+  )
+}
 
 export { SubscribeButton }
