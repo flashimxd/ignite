4541d53838d154b798669691803fc06de268fa5c
Rangel Netto <rangelnetto@sympla.com.br>
2021-07-12 17:22:20 -0300

    adding active links.

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
 import styles from './styles.module.scss';
+import Prismic from '@prismicio/client'
+import { GetStaticProps } from "next";
+import { RichText } from "prismic-dom";
 
-export default function Posts() {
+type Post = {
+  slug: string,
+  title: string,
+  excerpt: string,
+  updatedAt: string, 
+}
+
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -9,31 +24,45 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="/#">
-            <time>12 de abril de 2021</time>
-            <strong>Galo doido de BH</strong>
-            <p>
-              Galo doido de minas e o atletico mg galo doidooo galo de minas e o maior de todos os tempos
-            </p>
+         { posts.map(post => (
+           <a  key={post.slug} href="/#">
+            <time>{post.updatedAt}</time>
+            <strong>{post.title}</strong>
+            <p>{post.excerpt}</p>
           </a>
-
-          <a href="/#">
-            <time>12 de abril de 2021</time>
-            <strong>Galo doido de BH</strong>
-            <p>
-              Galo doido de minas e o atletico mg galo doidooo galo de minas e o maior de todos os tempos
-            </p>
-          </a>
-
-          <a href="/#">
-            <time>12 de abril de 2021</time>
-            <strong>Galo doido de BH</strong>
-            <p>
-              Galo doido de minas e o atletico mg galo doidooo galo de minas e o maior de todos os tempos
-            </p>
-          </a>
+         ))}
         </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], { 
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  })
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+  
+  return {
+    props: {
+      posts
+    }
+  }
 }
